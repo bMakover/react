184d86d3fba9b94d6527f732dcfc972a697ac45f
Batsheva Makover <batsheva4050@gmail.com>
2023-12-12 12:27:33 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,42 @@ import './App.css';
 import Counter from './counter';
 import Toggle from './toggle';
 import AppDate from './appDate';
-
+import {BrowserRouter,Routes,Route , Link} from "react-router-dom"
 function App() {
   return (
-    <div className="App">
-      <Counter />
-      <Toggle/>
-      <AppDate/>
+    <BrowserRouter>
+    <header>
+
+      <h2>My logo</h2>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">Home</Link>
+          </li>  
+           <li>
+            <Link to="/employee">Home</Link>
+          </li>
+          <li>
+            <Link to="/form">Home</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <Routes>
+<Route index element={<Toggle/>}/>
+<Route path="/about" element={<Toggle/>}/>
+<Route path="/employee"element={<AppDate/>}/>
+<Route path="/employee/:company" element={<Counter/>}/>
+{/* <Route path="/form" element={<Form/>}/> */}
+<Route path="*" element={<h2>Page not found</h2>}/>
+
+
+    </Routes>
     
-    </div>
+    </BrowserRouter>
   );
 }
 
