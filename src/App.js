af9e98cda8fcfe4ab6c468f69d9e7808550f34eb
Batsheva Makover <batsheva4050@gmail.com>
2023-12-17 11:33:14 +0200

    redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import './global.css';
-
+ import { configureStore } from "@reduxjs/toolkit";
+ import { Provider } from "react-redux";
 import Counter from './counter';
 import Toggle from './toggle';
 import AppDate from './appDate';
 import {BrowserRouter,Routes,Route , Link} from "react-router-dom"
+import counterSlice from "./features/counterSlice";
 
-
+const myStore = configureStore({
+  reducer: {
+    counterSlice
+  }
+})
 function App() {
   return (
     <BrowserRouter>
+    <Provider store={myStore}>
     <header>
 
       <h2>My logo</h2>
@@ -22,7 +29,7 @@ function App() {
             <Link to="/about">Home</Link>
           </li>  
            <li>
-            <Link to="/employee">Home</Link>
+            <Link to="/counter">counter</Link>
           </li>
           <li>
             <Link to="/form">Home</Link>
@@ -34,13 +41,14 @@ function App() {
 <Route index element={<Toggle/>}/>
 <Route path="/about" element={<Toggle/>}/>
 <Route path="/employee"element={<AppDate/>}/>
-<Route path="/employee/:company" element={<Counter/>}/>
+{/* <Route path="/employee/:company" element={<Counter/>}/> */}
 {/* <Route path="/form" element={<Form/>}/> */}
+<Route path="/counter" element={<Counter />} />
 <Route path="*" element={<h2>Page not found</h2>}/>
 
 
     </Routes>
-    
+    </Provider >
     </BrowserRouter>
   );
 }
--- src/counter.js
+++ src/counter.js
@@ -2,18 +2,12 @@
 import React, { useState } from 'react'
 
 export default function Counter() {
-    //   state = {counter:2};
-    // useState -> הוק שיודע לייצר סטייטי
-    // מחזיר מערך שאנחנו מפרקים אותו ל2 תאים
-    // תא ראשון יחזיר משתנה שמכיל את הערך של הסטייט 
-    // במקרה שלנו 4 , תא 2 מכיל פונקציה שיודעת
-    // להשפיע על הערך של הסטייט בתא 1 ככה שהכל
-    // יעבוד דרך הריאקט דום
+
     const [counter, setCounter] = useState(4);
     const [user, setUser] = useState("Maor");
 
     const onAdd1 = () => {
-        // this.setState({counter:this.state.counter+1})
+        
         setCounter(counter + 1);
         
         if (counter + 1 > 10) {
